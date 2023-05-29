eea336c33eb54d69355c2ed13a462399d0825114
Matti Pedersen <mrmattipants@gmail.com>
2023-05-29 15:52:07 -0500

    Add files via upload

---
+++ Lenovo_Commercial_Vantage/Install-LenovoCommercialVantage.ps1
@@ -0,0 +1,42 @@
+﻿$LenovoVantage = (Get-AppxPackage | Where-Object {$_.Name -eq "E046963F.LenovoSettingsforEnterprise"})
+
+If (!$LenovoVantage) {
+
+        $DependencyFolderPath = "$($PSScriptRoot)\Dependencies"
+        
+        $Dependencies = Get-ChildItem -Path $DependencyFolderPath -Filter "*.appx*" | Select-Object -ExpandProperty FullName
+
+        Add-AppxPackage -Path "$($PSScriptRoot)\E046963F.LenovoSettingsforEnterprise_10.2305.30.0_neutral_~_k1h2ywk1493x8.Msixbundle" -DependencyPath $Dependencies -Confirm:$False -ErrorAction SilentlyContinue
+       
+        Write-Host "The Lenovo Commercial Vantage Store App been Installed to this System" -ForegroundColor Green
+
+} Else {
+    
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft Store App is already Installed on this System" -ForegroundColor Yellow
+
+}
+
+$MsVcLibs = Get-AppxPackage "Microsoft.VCLibs.140.00" -ErrorAction SilentlyContinue | Where-Object {$_.Architecture -eq "X64"}
+        
+$MsNetRuntime = Get-AppxPackage "Microsoft.NET.Native.Runtime.2.2" -ErrorAction SilentlyContinue | Where-Object {$_.Architecture -eq "X64"}
+        
+$MsNetFramework = Get-AppxPackage "Microsoft.NET.Native.Framework.2.2" -ErrorAction SilentlyContinue | Where-Object {$_.Architecture -eq "X64"}
+
+If ($MsVcLibs -and $MsNetRuntime -and $MsNetFramework) {
+
+    Write-Host "All Lenovo Commercial Vantage Dependencies have been Installed to this System" -ForegroundColor Green
+
+} ElseIf (!$MsVcLibs) {
+
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft VCLibs v140.00 Dependency is Not Installed" -ForegroundColor Red
+
+} ElseIf (!$MsNetRuntime) {
+
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft NET Native Runtime v2.2 Dependency is Not Installed" -ForegroundColor Red
+
+} ElseIf (!$MsNetFramework) {
+
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft NET Native Framework v2.2 Dependency is Not Installed" -ForegroundColor Red
+
+}
+
---
+++ Lenovo_Commercial_Vantage/Remove-LenovoCommercialVantage.ps1
@@ -0,0 +1,41 @@
+﻿$LenovoBundle = Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -eq "E046963F.LenovoSettingsforEnterprise"}
+
+$LenovoMachineApp = (Get-AppxPackage -AllUsers -ErrorAction SilentlyContinue | Where-Object {$_.Name -eq "E046963F.LenovoSettingsforEnterprise"})
+
+$LenovoUserApp = (Get-AppxPackage -ErrorAction SilentlyContinue | Where-Object {$_.Name -eq "E046963F.LenovoSettingsforEnterprise"})
+
+If ($LenovoBundle) {
+
+        $LenovoBundle | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+        
+        Write-Host "The Lenovo Commercial Vantage Microsoft Store Package has been Successfully Unprovisioned on this System" -ForegroundColor Green
+
+} Else {
+    
+    Write-Host "The Lenovo Commercial Vantage Microsoft Store Package has Not been Provisioned on this System" -ForegroundColor Yellow
+
+}
+
+If ($LenovoMachineApp) {
+
+    $LenovoMachineApp | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+        
+    Write-Host "The Lenovo Commercial Vantage Microsoft Store Package has been Successfully Removed for All Users" -ForegroundColor Green
+            
+} Else {
+
+    Write-Host "The Lenovo Commercial Vantage Microsoft Store Package is Not Installed for All Users" -ForegroundColor Yellow
+
+}
+
+If ($LenovoUserApp) {
+
+        $LenovoUserApp | Remove-AppxPackage -ErrorAction SilentlyContinue
+       
+        Write-Host "The Lenovo Commercial Vantage Microsoft Store App been Removed from this User Account" -ForegroundColor Green
+
+} Else {
+    
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft Store App is Not Installed under this User Account" -ForegroundColor Yellow
+
+}
