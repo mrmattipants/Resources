834ee7247968ed5bda94df948259f301165872a9
Matti Pedersen <mrmattipants@gmail.com>
2023-05-29 15:57:26 -0500

    Add files via upload

---
+++ Lenovo_Commercial_Vantage/Provision-LenovoCommercialVantage.ps1
@@ -0,0 +1,53 @@
+﻿
+$LenovoVantage = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "E046963F.LenovoSettingsforEnterprise"}
+
+If (!$LenovoVantage) {
+
+        $DependencyFolderPath = "$($PSScriptRoot)\Dependencies"
+        
+        $Dependencies = Get-ChildItem -Path $DependencyFolderPath -Filter "*.appx*" | Select-Object -ExpandProperty FullName     
+
+        Add-AppxProvisionedPackage -Online -PackagePath "$($PSScriptRoot)\E046963F.LenovoSettingsforEnterprise_10.2305.30.0_neutral_~_k1h2ywk1493x8.Msixbundle" -DependencyPackagePath $Dependencies -SkipLicense -ErrorAction SilentlyContinue
+       
+        $LenovoVantage = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "E046963F.LenovoSettingsforEnterprise"}
+
+        If ($LenovoVantage) {
+       
+            Write-Host "The Lenovo Commercial Vantage Store Package been Successfully Provisioned on this System" -ForegroundColor Green
+
+        } Else {
+
+            Write-Host "The Lenovo Commercial Vantage Store Package Provisioning Process has Failed" -ForegroundColor Red
+
+        }
+
+} Else {
+    
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft Store Package is already Provisioned on this System" -ForegroundColor Yellow
+
+}
+
+$MsVcLibs = Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object {$_.PackageName -eq "Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe"}
+
+$MsNetRuntime = Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object {$_.PackageName -eq "Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe"}
+
+$MsNetFramework =  Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object {$_.PackageName -like "Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe"}
+
+If ($MsVcLibs -and $MsNetRuntime -and $MsNetFramework) {
+
+    Write-Host "All Lenovo Commercial Vantage Dependency Packages have been Successfully Provisioned on this System" -ForegroundColor Green
+
+} ElseIf (!$MsVcLibs) {
+
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft VCLibs v140.00 Dependency is Not Provisioned" -ForegroundColor Yellow
+
+} ElseIf (!$MsNetRuntime) {
+
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft NET Native Runtime v2.2 Dependency is Not Provisioned" -ForegroundColor Yellow
+
+} ElseIf (!$MsNetFramework) {
+
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft NET Native Framework v2.2 Dependency is Not Provisioned" -ForegroundColor Yellow
+
+}
+
---
+++ Lenovo_Commercial_Vantage/Provision-LenovoCommercialVantagePackage.ps1
@@ -0,0 +1,40 @@
+﻿$DependCount = 0
+
+$LenovoVantage = Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -eq "E046963F.LenovoSettingsforEnterprise"}
+
+If (!$LenovoVantage) {
+
+        $DependencyFolderPath = "$($PSScriptRoot)\Dependencies"
+        
+        $Dependencies = Get-ChildItem -Path $DependencyFolderPath -Filter "*.appx*" | Select-Object -ExpandProperty FullName
+
+        ForEach ($Dependency in $Dependencies) {
+
+            $DependState = Add-AppxProvisionedPackage -Online -PackagePath $Dependency -SkipLicense -ErrorAction SilentlyContinue
+ 
+            If ($DependState.Online -eq $True) { 
+                $DependCount = ($DependCount + 1) 
+            }
+        }
+       
+
+        Add-AppxProvisionedPackage -Online -PackagePath "$($PSScriptRoot)\E046963F.LenovoSettingsforEnterprise_10.2305.30.0_neutral_~_k1h2ywk1493x8.Msixbundle" -SkipLicense -ErrorAction SilentlyContinue
+       
+        Write-Host "The Lenovo Commercial Vantage Store Package been Installed on this System" -ForegroundColor Green
+
+} Else {
+    
+    Write-Host "The Lenovo Commercial Vantage Store Microsoft Store Package is already Installed on this System" -ForegroundColor Yellow
+
+}
+
+If ($DependCount -eq 3) {
+
+    Write-Host "All of the Lenovo Commercial Vantage Microsoft Store App Dependencies have been Successfully Provisioned" -ForegroundColor Green
+
+} Else {
+
+    Write-Host "One or More of the Lenovo Commercial Vantage Microsoft Store App Dependencies are Not Provisioned" -ForegroundColor Yellow    
+
+}
+
