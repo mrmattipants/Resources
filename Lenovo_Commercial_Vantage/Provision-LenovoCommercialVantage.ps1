d2f1c1952305afe3a415121ec268a338a482db98
Matti Pedersen <mrmattipants@gmail.com>
2023-06-03 20:44:34 -0500

    Update Provision-LenovoCommercialVantage.ps1

--- Lenovo_Commercial_Vantage/Provision-LenovoCommercialVantage.ps1
+++ Lenovo_Commercial_Vantage/Provision-LenovoCommercialVantage.ps1
@@ -13,17 +13,17 @@ If (!$LenovoVantage) {
 
         If ($LenovoVantage) {
        
-            Write-Host "The Lenovo Commercial Vantage Store Package been Successfully Provisioned on this System" -ForegroundColor Green
+            Write-Host "The Lenovo Commercial Vantage Microsoft Store Package been Successfully Provisioned on this System" -ForegroundColor Green
 
         } Else {
 
-            Write-Host "The Lenovo Commercial Vantage Store Package Provisioning Process has Failed" -ForegroundColor Red
+            Write-Host "The Lenovo Commercial Vantage Microsoft Store Package Provisioning Process has Failed" -ForegroundColor Red
 
         }
 
 } Else {
     
-    Write-Host "The Lenovo Commercial Vantage Store Microsoft Store Package is already Provisioned on this System" -ForegroundColor Yellow
+    Write-Host "The Lenovo Commercial Vantage Microsoft Store Package is already Provisioned on this System" -ForegroundColor Yellow
 
 }
 
@@ -34,20 +34,20 @@ $MsNetRuntime = Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
 $MsNetFramework =  Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object {$_.PackageName -like "Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe"}
 
 If ($MsVcLibs -and $MsNetRuntime -and $MsNetFramework) {
-
+ok
     Write-Host "All Lenovo Commercial Vantage Dependency Packages have been Successfully Provisioned on this System" -ForegroundColor Green
 
 } ElseIf (!$MsVcLibs) {
 
-    Write-Host "The Lenovo Commercial Vantage Store Microsoft VCLibs v140.00 Dependency is Not Provisioned" -ForegroundColor Yellow
+    Write-Host "The Lenovo Commercial Vantage Microsoft VCLibs v140.00 Dependency is Not Provisioned" -ForegroundColor Yellow
 
 } ElseIf (!$MsNetRuntime) {
 
-    Write-Host "The Lenovo Commercial Vantage Store Microsoft NET Native Runtime v2.2 Dependency is Not Provisioned" -ForegroundColor Yellow
+    Write-Host "The Lenovo Commercial Vantage Microsoft NET Native Runtime v2.2 Dependency is Not Provisioned" -ForegroundColor Yellow
 
 } ElseIf (!$MsNetFramework) {
 
-    Write-Host "The Lenovo Commercial Vantage Store Microsoft NET Native Framework v2.2 Dependency is Not Provisioned" -ForegroundColor Yellow
+    Write-Host "The Lenovo Commercial Vantage Microsoft NET Native Framework v2.2 Dependency is Not Provisioned" -ForegroundColor Yellow
 
 }
 
